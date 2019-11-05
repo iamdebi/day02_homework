e05fc84f6a36852095f8699bb6d2c78a50ce0dfe
Debbie <debiurquhart@live.co.uk>
2019-11-05 16:34:51 +0000

    adds most of code for numbers to month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -28,6 +28,75 @@ def join_string(string_1, string_2)
   return "#{string_1 }#{string_2}"
 end
 
-def add_string_as_number(string)
-  return string.to_1()
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i() + string_2.to_i()
 end
+
+def number_to_full_month_name(number_month)
+  case number_month
+  when 1
+    p "January"
+  when 2
+    p "Febuary"
+  when 3
+    p "March"
+  when 4
+    p "April"
+  when 5
+    p "May"
+  when 6
+    p "June"
+  when 7
+    p "July"
+  when 8
+    p "August"
+  when 9
+    p "September"
+  when 10
+    p "October"
+  when 11
+    p "Novermber"
+  when 12
+    p "December"
+end
+
+def number_to_short_month_name(number_month)
+  case number_month
+  when 1
+    p "Jan"
+  when 2
+    p "Feb"
+  when 3
+    p "Mar"
+  when 4
+    p "Apr"
+  when 5
+    p "May"
+  when 6
+    p "Jun"
+  when 7
+    p "Jul"
+  when 8
+    p "Aug"
+  when 9
+    p "Sep"
+  when 10
+    p "Oct"
+  when 11
+    p "Nov"
+  when 12
+    p "Dec"
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+  end

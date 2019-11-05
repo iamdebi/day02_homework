2a2ad9821f030a6a19a179903a2d9d50edb8a780
Debbie <debiurquhart@live.co.uk>
2019-11-05 15:50:17 +0000

    initial git add' gti status git status exit pwd
    quit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,17 @@
+
+def return_10
+  return 10
+end
+
+
+def add(number_one, number_two)
+  return number_one + number_two
+end
+
+def subtract(number_one, number_two)
+  return number_one - number_two
+end
+
+def  multiply(number_one, number_two)
+  return number_one * number_two
+end

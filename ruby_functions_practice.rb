6215e5c6718dfb25e4b638f18d782a3778c3a7fc
Debbie <debiurquhart@live.co.uk>
2019-11-05 15:59:58 +0000

    adds join_string

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -15,3 +15,15 @@ end
 def  multiply(number_one, number_two)
   return number_one * number_two
 end
+
+def divide(number_one, number_two)
+  return number_one / number_two
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return "#{string_1 }#{string_2}"
+end
